1218d0d8d803541af5d5db595fcd40aaba6cbddb
eCostaRibeiro <eduardo.costa.ribeiro@live.com>
2019-03-19 11:39:14 -0300

    testando a página Elm

---
+++ index.elm
@@ -0,0 +1,21 @@
+import Browser
+import Html exposing (Html, , div, text)
+
+
+main =
+  Browser.sandbox { init = 0, update = update, view = view }
+
+type Msg = Increment | Decrement
+
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+view model =
+  div []
+    [ text ("testando")
+    ]
